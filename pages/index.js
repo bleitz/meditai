52939c90f6c9a969b979262bdc1a78bf2c1d2225
bleitz <39960235+bleitz@users.noreply.github.com>
2023-03-13 17:52:26 +0100

    Tweaks UI, adds loading animation and music

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import Head from "next/head";
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import styles from "./index.module.css";
 
 import ReactAudioPlayer from "react-audio-player";
-import { Text, Textarea, Button, Switch, Radio } from '@nextui-org/react';
+import { Text, Textarea, Button, Radio, Image } from '@nextui-org/react';
 
 import Lottie from "lottie-react";
 import loadingAnimation from "../public/circle-animation.json";
@@ -13,7 +13,32 @@ export default function Home() {
   const [audioSrc, setAudioSrc] = useState('');
   const [duration, setDuration] = useState("5");
   const [loading, setLoading] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [volume, setVolume] = useState(0.2);
 
+  const audioRef = useRef(null);
+
+  const togglePlay = () => {
+    const audio = audioRef.current;
+    if (isPlaying) {
+      audio.pause();
+    } else {
+      audio.play();
+    }
+    setIsPlaying(!isPlaying);
+  };  
+
+  const handleVolumeChange = (e) => {
+    const audio = audioRef.current;
+    audio.volume = e.target.value;
+    setVolume(e.target.value);
+  };
+
+  useEffect(() => {
+    audioRef.current.volume = volume;
+  }, [volume]);
+
+  
   
   async function onSubmit(event) {
 
@@ -92,15 +117,44 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>mindGPT - 100% AI geer</title>
-        <link rel="icon" href="/dog.png" />
+        <title>mindGPT - 100% AI-generated, personalized guided meditations</title>
+        <link rel="icon" href="/logo-noname.png" />
       </Head>
 
-      <main className={styles.main}>
-        <Text h2>How can I guide your meditation?</Text>
-        <Text style={{ "margin": "24px 0"}}>Do you want to cultivate a feeling & thought? Contemplate a topic? Manifest or internalise an idea?</Text>
+      <div style={{ "display": "flex", "justifyContent": "center", "alignItems": "center"}}>
+          <label htmlFor="music" style={{ "margin": "16px"}}>Music</label>
+          <input
+            id="music"
+            type="range"
+            min="0"
+            max="1"
+            step="0.1"
+            value={volume}
+            onChange={handleVolumeChange}
+            disabled={!isPlaying}
+          />
+          <audio loop src="music.mp3" ref={audioRef}></audio>
+      </div>
 
-        <div style={{ "marginBottom": "40px"}}>
+      <main className={styles.main}>
+
+        <div style={{ "margin": "16px 0 32px" }}>
+          <Image
+            width={150}
+            height={150}  
+            src="logo-noname.png"
+            alt="Logo"
+            objectFit="cover"
+          />
+        </div>
+
+        <div style={{ "margin": "16px 0 48px" }}>
+          <Text h2>How can I guide your meditation?</Text>
+          <Text>Do you want to cultivate a specific thought or feeling? <br></br> Contemplate a particular topic?<br></br>  Manifest or internalise a concrete idea?</Text>
+        </div>
+
+        <div style={{ "margin": "16px 0 32px", "width": "100%" }}>
+
           <Textarea
             placeholder="e.g. I want to cultivate more positivity towards my future"
             value={topicInput}
@@ -108,42 +162,35 @@ export default function Home() {
             fullWidth={true}
           />
 
-          <Radio.Group  
-            orientation="horizontal" 
-            label="Time" 
-            defaultValue="5" 
-            value={duration} 
-            onChange={setDuration}
-            style={{ "margin": "24px 0"}}
-          >
-            <Radio value="5">
-              5 min
-            </Radio>
-            <Radio value="10">
-              10 min
-            </Radio>
-            <Radio value="15">
-              15 min
-            </Radio>
-          </Radio.Group>
-        </div>
+          <div style={{ "margin": "16px 0 16px", "display": "flex", "justifyContent": "center" }}>
+            <Radio.Group  
+              orientation="horizontal" 
+              defaultValue="5" 
+              value={duration} 
+              onChange={setDuration}
+            >
+              <Radio value="5">
+                5 min
+              </Radio>
+              <Radio value="10">
+                10 min
+              </Radio>
+              <Radio value="15">
+                15 min
+              </Radio>
+            </Radio.Group>
+          </div>
 
-        <div style={{ "margin": "24px 0"}}>
-          {
-            audioSrc ? <ReactAudioPlayer src={audioSrc} autoplay controls /> 
-            :
-              (loading ? <Lottie style={{ "width": "100px" }} animationData={loadingAnimation} loop={true} />
+          <div style={{ "height": "100px", "margin": "8px 0 16px", "display": "flex", "justifyContent": "center", "alignItems": "center" }}>
+            {
+              audioSrc ? <ReactAudioPlayer src={audioSrc} controls onPlay={togglePlay} onPause={togglePlay}/> 
               :
-              <Button onPress={onSubmit}>Generate your meditation</Button>)
-          }
+                (loading ? <Lottie style={{ "width": "100px" }} animationData={loadingAnimation} loop={true} />
+                :
+                <Button onPress={onSubmit}>Generate your meditation</Button>)
+            }
+          </div>
         </div>
-{/*         <div className="music" style={{"display": "flex"}}>
-          <Switch 
-            checked={true}
-            size="sm" 
-          />
-          <Text style={{ "margin": "0 8px"}}>Music</Text>
-        </div> */}
         
       </main>
     </div>
