3fea842347b778731244b0b769a8b52c719e1f46
bleitz <39960235+bleitz@users.noreply.github.com>
2023-03-15 08:00:39 +0100

    Fix GA

--- pages/_document.js
+++ pages/_document.js
@@ -20,7 +20,7 @@ class MyDocument extends Document {
 
           {/* Global Site Tag (gtag.js) - Google Analytics */}
           <Script
-            src="https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}"
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
             strategy="afterInteractive"
           />
           <Script id="google-analytics" strategy="afterInteractive">
