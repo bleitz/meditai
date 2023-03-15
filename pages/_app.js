54b1e2bc11a7cb7a57d31042c5f366039516ab30
bleitz <39960235+bleitz@users.noreply.github.com>
2023-03-15 08:41:24 +0100

    Fix GA

--- pages/_app.js
+++ pages/_app.js
@@ -28,24 +28,20 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
+
       {/* Global Site Tag (gtag.js) - Google Analytics */}
       <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`} strategy="afterInteractive"/>
-      <Script
-        id='google-analytics'
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-          }}
-      />
+      <Script strategy="lazyOnload">
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+      </Script>
     
-      // 2. Use at the root of your app
       <NextUIProvider>
 
         <Component {...pageProps} />
